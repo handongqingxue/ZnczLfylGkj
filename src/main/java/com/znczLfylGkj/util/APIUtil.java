a35f096394e98cec609a86725b246c8ce0ffea11
handongqingxue <1417140290@qq.com>
2022-01-05 14:59:22 +0800

    完善

--- src/main/java/com/znczLfylGkj/util/APIUtil.java
+++ src/main/java/com/znczLfylGkj/util/APIUtil.java
@@ -13,14 +13,16 @@ import java.util.Map.*;
 import org.json.JSONObject;
 
 public class APIUtil {
+	
+	public static final String SERVICE_URL="http://localhost:8080/ZnczLfyl/gkj/";
 
 	//https://www.cnblogs.com/aeolian/p/7746158.html
 	//https://www.cnblogs.com/bobc/p/8809761.html
-	public void doHttp(String requestUrl, Map<String, String> params) throws IOException {
+	public static JSONObject doHttp(String method, Map<String, Object> params) throws IOException {
 		// 构建请求参数  
         StringBuffer paramsSB = new StringBuffer();
 		if (params != null) {  
-            for (Entry<String, String> e : params.entrySet()) {
+            for (Entry<String, Object> e : params.entrySet()) {
             	paramsSB.append(e.getKey());  
             	paramsSB.append("=");  
             	paramsSB.append(e.getValue());  
@@ -31,8 +33,8 @@ public class APIUtil {
 		
 		StringBuffer sbf = new StringBuffer(); 
 		String strRead = null; 
-		URL url = new URL(requestUrl);
-		HttpURLConnection connection = (HttpURLConnection)url.openConnection(); 
+		URL url = new URL(SERVICE_URL+method);
+		HttpURLConnection connection = (HttpURLConnection)url.openConnection();
 		connection.setRequestMethod("POST");//请求post方式
 		connection.setDoInput(true); 
 		connection.setDoOutput(true); 
@@ -56,17 +58,55 @@ public class APIUtil {
 		connection.disconnect();
 		String result = sbf.toString();
 		System.out.println("result==="+result);
+		JSONObject resultJO = new JSONObject(result);
+		return resultJO;
 	}
-	
-	public static void main(String[] args) {
-		APIUtil a=new APIUtil();
+
+	public static JSONObject getDingDan() {
+		JSONObject resultJO = null;
 		try {
-			Map parames = new HashMap<String, String>();  
-	        parames.put("mm", "aaazz");  
-			a.doHttp("http://localhost:8080/ZnczLfyl/gkj/aaa",parames);
+			Map parames = new HashMap<String, String>();
+	        parames.put("cph", "9008");  
+	        parames.put("ddztMc", "已审核");
+	        resultJO = APIUtil.doHttp("getDingDan",parames);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		finally {
+			return resultJO;
+		}
+	}
+	
+	public static JSONObject newBangDanJiLu(Integer ddId) {
+		JSONObject resultJO = null;
+		try {
+			Map parames = new HashMap<String, String>();
+	        parames.put("ddId", ddId);  
+	        resultJO = APIUtil.doHttp("newBangDanJiLu",parames);
+			System.out.println("33333333="+resultJO.toString());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		finally {
+			return resultJO;
+		}
+	}
+	
+	public static void main(String[] args) {
+		JSONObject resultJO=APIUtil.getDingDan();
+        if("ok".equals(resultJO.getString("status"))) {
+        	JSONObject ddJO=resultJO.getJSONObject("dingDan");
+        	System.out.println("ddId==="+ddJO.getInt("id"));
+        	JSONObject bdJO=APIUtil.newBangDanJiLu(ddJO.getInt("id"));
+        	System.out.println("bdJO==="+bdJO.toString());
+        }
+        else {
+        	System.out.println("message==="+resultJO.getString("message"));
+        }
+		
+		//这里是磅房外的车牌识别摄像头抓拍到车牌号后回调的方法，回调时根据抓拍到的车牌号找到状态是排队中的订单（每次只能获得一条订单，根据上一部审核的先后顺序，先来后到，先审核的先进行车牌识别）
+		//识别后根据抓拍到的订单号与订单里的订单号对比，一致的话抬起道闸，订单状态变为一检上磅
 	}
 }
