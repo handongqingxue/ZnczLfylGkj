d68c3f134a18afbea781aa7987baed35b7c7f9a7
handongqingxue <1417140290@qq.com>
2022-02-09 11:00:47 +0800

    完善

--- src/main/java/com/znczLfylGkj/yz/ZhiLingUtil.java
+++ src/main/java/com/znczLfylGkj/yz/ZhiLingUtil.java
@@ -12,10 +12,120 @@ public class ZhiLingUtil {
 	
 	/**
 	 * 
+	 * @return  读取成功请
+	 */
+	public static String get82() {
+		return getByDuanHao(82);
+	}
+	
+	/**
+	 * 
 	 * @return  请车牌号为
 	 */
 	public static String get83() {
 		return getByDuanHao(83);
+	}
+	
+	/**
+	 * 
+	 * @return  身份识别已成功请拿走身份证
+	 */
+	public static String get84() {
+		return getByDuanHao(84);
+	}
+	
+	/**
+	 * @return  请等待
+	 */
+	public static String get85() {
+		return getByDuanHao(85);
+	}
+	
+	/**
+	 * @return  没有找到匹配订单
+	 */
+	public static String get86() {
+		return getByDuanHao(86);
+	}
+	
+	/**
+	 * 
+	 * @return  请上磅把车辆停放在地磅中间
+	 */
+	public static String get87() {
+		return getByDuanHao(87);
+	}
+	
+	/**
+	 * 
+	 * @return  停车熄火等待称重
+	 */
+	public static String get88() {
+		return getByDuanHao(88);
+	}
+	
+	/**
+	 * 
+	 * @return  称重完成请下磅
+	 */
+	public static String get89() {
+		return getByDuanHao(89);
+	}
+	
+	/**
+	 * 
+	 * @return  请确保车辆停在磅秤中间
+	 */
+	public static String get90() {
+		return getByDuanHao(90);
+	}
+	
+	/**
+	 * 
+	 * @return  车牌识别失败，请联系相关人员
+	 */
+	public static String get91() {
+		return getByDuanHao(91);
+	}
+	
+	/**
+	 * 
+	 * @return  车牌未识别，请移动车辆位置
+	 */
+	public static String get92() {
+		return getByDuanHao(92);
+	}
+	
+	/**
+	 * 
+	 * @return  红外设备故障，请联系相关人员
+	 */
+	public static String get93() {
+		return getByDuanHao(93);
+	}
+	
+	/**
+	 * 
+	 * @return  红外识别有误， 请联系管理人员
+	 */
+	public static String get94() {
+		return getByDuanHao(94);
+	}
+	
+	/**
+	 * 
+	 * @return  称重失败， 请联系相关人员
+	 */
+	public static String get95() {
+		return getByDuanHao(95);
+	}
+	
+	/**
+	 * 
+	 * @return  车牌号和订单不一致， 请联系相关人员
+	 */
+	public static String get96() {
+		return getByDuanHao(96);
 	}
 
 	public static String getByDuanHao(int num) {
@@ -52,16 +162,262 @@ public class ZhiLingUtil {
 			zlStr="F0 01 06 04 0A 05";
 			break;
 		case 11:
-			zlStr="";
+			zlStr="F0 01 06 04 0B 06";
+			break;
+		case 12:
+			zlStr="F0 01 06 04 0C 07";
+			break;
+		case 13:
+			zlStr="F0 01 06 04 0D 08";
+			break;
+		case 14:
+			zlStr="F0 01 06 04 0E 09";
+			break;
+		case 15:
+			zlStr="F0 01 06 04 0F 0A";
+			break;
+		case 16:
+			zlStr="F0 01 06 04 10 0B";
+			break;
+		case 17:
+			zlStr="F0 01 06 04 11 0C";
+			break;
+		case 18:
+			zlStr="F0 01 06 04 12 0D";
+			break;
+		case 19:
+			zlStr="F0 01 06 04 13 0E";
+			break;
+		case 20:
+			zlStr="F0 01 06 04 14 0F";
+			break;
+		case 21:
+			zlStr="F0 01 06 04 15 10";
+			break;
+		case 22:
+			zlStr="F0 01 06 04 16 11";
+			break;
+		case 23:
+			zlStr="F0 01 06 04 17 12";
+			break;
+		case 24:
+			zlStr="F0 01 06 04 18 13";
+			break;
+		case 25:
+			zlStr="F0 01 06 04 19 14";
+			break;
+		case 26:
+			zlStr="F0 01 06 04 1A 15";
+			break;
+		case 27:
+			zlStr="F0 01 06 04 1B 16";
+			break;
+		case 28:
+			zlStr="F0 01 06 04 1C 17";
+			break;
+		case 29:
+			zlStr="F0 01 06 04 1D 18";
+			break;
+		case 30:
+			zlStr="F0 01 06 04 1E 19";
+			break;
+		case 31:
+			zlStr="F0 01 06 04 1F 1A";
+			break;
+		case 32:
+			zlStr="F0 01 06 04 20 1B";
+			break;
+		case 33:
+			zlStr="F0 01 06 04 21 1C";
+			break;
+		case 34:
+			zlStr="F0 01 06 04 22 1D";
+			break;
+		case 35:
+			zlStr="F0 01 06 04 23 1E";
+			break;
+		case 36:
+			zlStr="F0 01 06 04 24 1F";
+			break;
+		case 37:
+			zlStr="F0 01 06 04 25 20";
+			break;
+		case 38:
+			zlStr="F0 01 06 04 26 21";
+			break;
+		case 39:
+			zlStr="F0 01 06 04 27 22";
+			break;
+		case 40:
+			zlStr="F0 01 06 04 28 23";
+			break;
+		case 41:
+			zlStr="F0 01 06 04 29 24";
+			break;
+		case 42:
+			zlStr="F0 01 06 04 2A 25";
+			break;
+		case 43:
+			zlStr="F0 01 06 04 2B 26";
+			break;
+		case 44:
+			zlStr="F0 01 06 04 2C 27";
+			break;
+		case 45:
+			zlStr="F0 01 06 04 2D 28";
+			break;
+		case 46:
+			zlStr="F0 01 06 04 2E 29";
+			break;
+		case 47:
+			zlStr="F0 01 06 04 2F 2A";
+			break;
+		case 48:
+			zlStr="F0 01 06 04 30 2B";
+			break;
+		case 49:
+			zlStr="F0 01 06 04 31 2C";
+			break;
+		case 50:
+			zlStr="F0 01 06 04 32 2D";
+			break;
+		case 51:
+			zlStr="F0 01 06 04 33 2E";
+			break;
+		case 52:
+			zlStr="F0 01 06 04 34 2F";
+			break;
+		case 53:
+			zlStr="F0 01 06 04 35 30";
 			break;
 		case 54:
 			zlStr="F0 01 06 04 36 31";
 			break;
+		case 55:
+			zlStr="F0 01 06 04 37 32";
+			break;
+		case 56:
+			zlStr="F0 01 06 04 38 33";
+			break;
+		case 57:
+			zlStr="F0 01 06 04 39 34";
+			break;
+		case 58:
+			zlStr="F0 01 06 04 3A 35";
+			break;
+		case 59:
+			zlStr="F0 01 06 04 3B 36";
+			break;
+		case 60:
+			zlStr="F0 01 06 04 3C 37";
+			break;
+		case 61:
+			zlStr="F0 01 06 04 3D 38";
+			break;
+		case 62:
+			zlStr="F0 01 06 04 3E 39";
+			break;
+		case 63:
+			zlStr="F0 01 06 04 3F 3A";
+			break;
+		case 64:
+			zlStr="F0 01 06 04 40 3B";
+			break;
+		case 65:
+			zlStr="F0 01 06 04 41 3C";
+			break;
+		case 66:
+			zlStr="F0 01 06 04 42 3D";
+			break;
+		case 67:
+			zlStr="F0 01 06 04 43 3E";
+			break;
+		case 68:
+			zlStr="F0 01 06 04 44 3F";
+			break;
+		case 69:
+			zlStr="F0 01 06 04 45 40";
+			break;
+		case 70:
+			zlStr="F0 01 06 04 46 41";
+			break;
+		case 71:
+			zlStr="F0 01 06 04 47 42";
+			break;
+		case 72:
+			zlStr="F0 01 06 04 48 43";
+			break;
+		case 73:
+			zlStr="F0 01 06 04 49 44";
+			break;
+		case 74:
+			zlStr="F0 01 06 04 4A 45";
+			break;
+		case 75:
+			zlStr="F0 01 06 04 4B 46";
+			break;
+		case 76:
+			zlStr="F0 01 06 04 4C 47";
+			break;
+		case 77:
+			zlStr="F0 01 06 04 4D 48";
+			break;
+		case 78:
+			zlStr="F0 01 06 04 4E 49";
+			break;
+		case 79:
+			zlStr="F0 01 06 04 4F 4A";
+			break;
+		case 80:
+			zlStr="F0 01 06 04 50 4B";
+			break;
 		case 81:
 			zlStr="F0 01 06 04 51 4C";
 			break;
+		case 82:
+			zlStr="F0 01 06 04 52 4D";
+			break;
 		case 83:
 			zlStr="F0 01 06 04 53 4E";
+			break;
+		case 84:
+			zlStr="F0 01 06 04 54 4F";
+			break;
+		case 85:
+			zlStr="F0 01 06 04 55 50";
+			break;
+		case 86:
+			zlStr="F0 01 06 04 56 51";
+			break;
+		case 87:
+			zlStr="F0 01 06 04 57 52";
+			break;
+		case 88:
+			zlStr="F0 01 06 04 58 53";
+			break;
+		case 89:
+			zlStr="F0 01 06 04 59 54";
+			break;
+		case 90:
+			zlStr="F0 01 06 04 5A 55";
+			break;
+		case 91:
+			zlStr="F0 01 06 04 5B 56";
+			break;
+		case 92:
+			zlStr="F0 01 06 04 5C 57";
+			break;
+		case 93:
+			zlStr="F0 01 06 04 5D 58";
+			break;
+		case 94:
+			zlStr="F0 01 06 04 5E 59";
+			break;
+		case 95:
+			zlStr="F0 01 06 04 5F 5A";
+			break;
+		case 96:
+			zlStr="F0 01 06 04 60 5B";
 			break;
 		}
 		return zlStr;
