b1eacc9ea87d592a5c110e173a59eff5bdcbd114
handongqingxue <1417140290@qq.com>
2022-02-15 15:56:58 +0800

    完善

--- src/main/java/com/znczLfylGkj/task/StartTask.java
+++ src/main/java/com/znczLfylGkj/task/StartTask.java
@@ -20,11 +20,11 @@ public class StartTask {
 		//CpsbsxtTask cpsbsxtTask = new CpsbsxtTask();
 		//cpsbsxtTask.start();
 		
-		YiJianJdq yjjdq=new YiJianJdq();
-		JdqZlUtil.setYjjdq(yjjdq);
+		//YiJianJdq yjjdq=new YiJianJdq();
+		//JdqZlUtil.setYjjdq(yjjdq);
 		
-		//ErJianJdq ejjdq=new ErJianJdq();
-		//JdqZlUtil.setEjjdq(ejjdq);
+		ErJianJdq ejjdq=new ErJianJdq();
+		JdqZlUtil.setEjjdq(ejjdq);
 
 		//testLiuCheng();
 		
